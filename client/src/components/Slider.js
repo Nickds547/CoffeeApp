3c0526b8348840e819c3beab20b6033fa0d7da4b
Nickds54 <nicksalter98@gmail.com>
2019-12-13 11:05:04 -0600

    Added working slider

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,23 +1,51 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import {Gallery, GalleryImage } from 'react-gesture-gallery';
-import data from '../FakeJSON/test.json';
+import '../App.css';
+import styled from 'styled-components';
+import counter from '../assets/Brewhouse_counter.jpg';
+import mug from '../assets/coffee_mugs.jpg';
+import food from '../assets/Food.jpg';
+import out from '../assets/Outside.jpg';
 
-const images = data.images;
-
+const Wrapper = styled.div`
+height: 65vh;
+width:100vw;
+`;
 
 const Slider = () =>{
 
-    const [num, setNum] = useState(0);
+    const images = [counter,food,mug,out];
+    const [index, setIndex] = useState(0);
 
-    console.log(images[num].src);
+    useEffect(()=>{
+
+        const interval = setInterval(()=> {
+
+            if(index === images.length -1){
+                setIndex(0);
+            }
+
+            else{
+                setIndex(index + 1)
+            }
+
+        },3000)
+
+        return() => clearInterval(interval);
+
+    }, [index])
 
     return(
-        <Gallery index={num}>
-            {images.map(() => (
-                <GalleryImage src={images[num].src}/>
-            ))}
-            
-        </Gallery>
+        
+        <Wrapper>
+            <Gallery className="slider-image" index={index} onRequestChange={i =>{setIndex(i);}}>
+                {images.map(() => (
+                    <GalleryImage className="image-gallery-slide" objectFit="contain" src={images[index]}/>
+                ))}
+                
+            </Gallery>
+        
+        </Wrapper>
     )
 }
 
